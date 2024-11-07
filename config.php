c68920886f13cf8318c6fab2b2dbd3532c59a2ac
Goodman Luphondo <luphondog@icloud.com>
2024-11-07 07:56:36 +0200

    Credentials Updates For Heroku

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ ini_set("log_errors", 1);
 ini_set("display_errors", "On");
 ini_set("error_log", "logs/error.log");
 
-define('APP_URL', 'https://clctest.php');
+define('APP_URL', 'https://clctest.goodmanmade.me');
 define('APP_NAME', 'CLC Test');
 
 define('METADATA_TITLE', 'CLC Test');
@@ -13,10 +13,10 @@ define('METADATA_CONTENT', 'CLC Test');
 define('METADATA_DESCRIPTION', 'CLC Test');
 
 define('DB_CONNECTION', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_DATABASE', 'test');
-define('DB_USERNAME', 'goodman');
-define('DB_PASSWORD', '');
+define('DB_HOST', 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_DATABASE', 'ydonc5vq505z74c2');
+define('DB_USERNAME', 'q38kvgujl2jic7ra');
+define('DB_PASSWORD', 'j8dl82gbyptoen1m');
 
 define('RECAPTCHA_SITE_KEY', '6LfRSXcqAAAAAJUJ5qJtIX7xgKwa76zuhCi61VPa');
 define('RECAPTCHA_SECRET_KEY', '6LfRSXcqAAAAALvtPyDsYP9PIRV1yNX1f2JdAdQ-');
